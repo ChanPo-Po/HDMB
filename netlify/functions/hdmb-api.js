9193e51e813c9bbafb54bdf7713f6ff3370656e4
ChanPo-Po <tp139365@gmail.com>
2026-06-10 19:01:22 +0700

    Update hdmb-api.js

--- netlify/functions/hdmb-api.js
+++ netlify/functions/hdmb-api.js
@@ -1,1 +1,48 @@
+const GAS_URL = "https://script.google.com/macros/s/AKfycbz73hl3p-vRrwGumzuBOmfryYFtt_QXLzxqDkbtMCQKp1E6ijRDJMF-JIueSKCGMw4C/exec";
 
+exports.handler = async function(event) {
+  if (event.httpMethod === "OPTIONS") {
+    return {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers": "Content-Type",
+        "Access-Control-Allow-Methods": "POST, OPTIONS"
+      },
+      body: ""
+    };
+  }
+
+  try {
+    const res = await fetch(GAS_URL, {
+      method: "POST",
+      body: event.body,
+      headers: {
+        "Content-Type": "text/plain;charset=utf-8"
+      }
+    });
+
+    const text = await res.text();
+
+    return {
+      statusCode: 200,
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*"
+      },
+      body: text
+    };
+  } catch (err) {
+    return {
+      statusCode: 500,
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*"
+      },
+      body: JSON.stringify({
+        ok: false,
+        message: String(err)
+      })
+    };
+  }
+};
